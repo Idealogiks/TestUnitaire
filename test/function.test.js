774d92d2165c83285ef1beadb987e2aaa477d0e0
Idea <bernard.gladys22@gmail.com>
2025-02-05 09:54:32 +0100

    début fonction price

--- test/function.test.js
+++ test/function.test.js
@@ -14,5 +14,26 @@ describe("when i want to verify if a number is even or not", () => {
     test("then it shouldn't return error for number", () => {
         expect(() => isEven(2)).not.toThrow('Input must be a number');
     });
+})
 
+describe("when i want to calculate the price", () => {
+    test("then it throw an error when price isn't array ", () => {
+        expect(() => calculateTotalPrice(2)).toThrow('Prices must be an array');
+    }); 
+    test("then it not throw an error when price is an array ", () => {
+        expect(() => calculateTotalPrice([2])).not.toThrow('Prices must be an array');
+    });
+    test("then it not throw an error when tax is an number ", () => {
+        expect(() => calculateTotalPrice(2)).not.toThrow('Tax rate must be a number');
+    });
+    test("then it throw an error when tax isn't an number ", () => {
+        expect(() => calculateTotalPrice([2])).toThrow('Tax rate must be a number');
+    });
+    test("then it throw an error when price is negative ", () => {
+        const price = [-2];
+        expect(() => calculateTotalPrice()).toThrow('Each price must be a non-negative number');
+    });
+    test("then it throw an error when price is negative ", () => {
+        expect(() => calculateTotalPrice.price([-2])).toThrow('Each price must be a non-negative number');
+    });
 })
