506d2e549113472a196a5f4ccd52d1e432894dd4
elias hamidi <hamidielias59@gmail.com>
2025-02-05 11:45:25 +0100

    test purchase et test generate password

--- test/function.test.js
+++ test/function.test.js
@@ -64,3 +64,78 @@ describe("When i want to send a notification", () => {
     });
 });
 
+describe("processPurchase", () => {
+    test("should throw an error if the input is not an array", () => {
+        expect(() => processPurchase(6, 6)).toThrow("Prices must be an array");
+    });
+  
+    test("should throw an error if the tax rate is not a number", () => {
+        expect(() => processPurchase([6], "0.2")).toThrow("Tax rate must be a number");
+    });
+  
+    test("should throw an error if an element in the array is not a number", () => {
+        expect(() => processPurchase(["6"], 0.2)).toThrow("Each price must be a non-negative number");
+    });
+  
+    test("should throw an error if an element in the array is a negative number", () => {
+        expect(() => processPurchase([-6], 0.2)).toThrow("Each price must be a non-negative number");
+    });
+  
+    test("should return the correct total price", () => {
+        expect(processPurchase([6], 0.2)).toBe(7.2);
+    });
+  
+    test("should not return an incorrect total price", () => {
+        expect(processPurchase([6], 0.2)).not.toBe(1.6);
+    });
+  
+    test("should log the correct message", () => {
+        const spy = jest.spyOn(console, "log");
+        const prixTotal = processPurchase([5], 0.2);
+  
+        expect(spy).toHaveBeenCalledWith(
+            `Notification envoyée : Votre total est de ${prixTotal.toFixed(2)} €`
+        );
+        spy.mockRestore();
+    });
+  });
+  
+  
+  describe("generatePassword", () => {
+    test("should throw an error if length is not a number", () => {
+        expect(() => generatePassword("10", {})).toThrow("Length must be a number greater than or equal to 6");
+    });
+  
+    test("should throw an error if length is less than 6", () => {
+        expect(() => generatePassword(5, {})).toThrow("Length must be a number greater than or equal to 6");
+    });
+  
+    test("should generate a password of the correct length", () => {
+        const password = generatePassword(12, { uppercase: true, numbers: true, specialChars: true });
+        expect(password.length).toBe(12);
+    });
+  
+    test("should contain at least one uppercase letter if enabled", () => {
+        const password = generatePassword(10, { uppercase: true, numbers: false, specialChars: false });
+        expect(/[A-Z]/.test(password)).toBe(true);
+    });
+  
+    test("should contain at least one number if enabled", () => {
+        const password = generatePassword(10, { uppercase: false, numbers: true, specialChars: false });
+        expect(/[0-9]/.test(password)).toBe(true);
+    });
+  
+    test("should contain at least one special character if enabled", () => {
+        const password = generatePassword(10, { uppercase: false, numbers: false, specialChars: true });
+        expect(/[!@#$%^&*()_+\[\]{}|;:,.<>?]/.test(password)).toBe(true);
+    });
+  
+    test("should not generate the same password twice in a row", () => {
+        const password1 = generatePassword(10, { uppercase: true, numbers: true, specialChars: true });
+        const password2 = generatePassword(10, { uppercase: true, numbers: true, specialChars: true });
+        expect(password1).not.toBe(password2);
+    });
+  });
+  
+  
+  
