b65adfc2cc1e2de0d73c8b24605b2be7254dfe26
Idea <bernard.gladys22@gmail.com>
2025-02-05 08:48:46 +0100

    Exo1: test even number/test odd number/test not a number/test is a number

--- test/function.test.js
+++ test/function.test.js
@@ -1,5 +1,18 @@
 const { isEven, calculateTotalPrice, processPurchase, sendNotification, generatePassword } = require("./../src/function.js");
 
-test("",() =>{
 
-});
+describe("when i want to verify if a number is even or not", () => {
+    test("then it should return true for even number ", () => {
+        expect(isEven(2)).toBe(true)
+    }); 
+    test("then it should return false for odd number", () => {
+        expect(isEven(1)).toBe(false)
+    });
+    test("then it should return error for not number", () => {
+        expect(() => isEven("2")).toThrow('Input must be a number');
+    });
+    test("then it shouldn't return error for number", () => {
+        expect(() => isEven(2)).not.toThrow('Input must be a number');
+    });
+
+})
